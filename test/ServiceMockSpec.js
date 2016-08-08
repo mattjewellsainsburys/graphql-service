8bcd5bb23f5ba07d89916d4839871dd472c55213
Matt Jewell <matt.jewell@sainsburys.co.uk>
2016-08-08 16:24:00 +0100

    passing tests

--- test/ServiceMockSpec.js
+++ test/ServiceMockSpec.js
@@ -9,11 +9,5 @@ describe('Schema', () => {
     const expected = 'world';
     const actual = response.data.hello;
     assert.equal(actual, expected);
-  })
-  it('fails at not being in the world', async function () {
-    const response = await graphql(schema, '{ hello }');
-    const expected = 'Mars';
-    const actual = response.data.hello;
-    assert.equal(actual, expected);
-  })
+  });
 });
